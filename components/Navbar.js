9b070e198f646a2fb496f9e7b29bebd566264a3b
bharathsurya021 <bharathsurya021@gmail.com>
2024-07-26 15:37:21 +0530

    remove nav links and add home route using Link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Navbar = () => {
         return (
                 <div className='flex py-3 flex-wrap justify-around'>
-                        <h1 className='text-lg font-semibold'>Todo App</h1>
-                        <ul className='flex gap-[40px] text-m'>
+                        <Link href={"/"}>
+                                <h1 className='text-lg font-semibold'>Todo App</h1>
+                        </Link>
+                        {/* <ul className='flex gap-[40px] text-m'>
                                 <li>Home</li>
                                 <li>Products</li>
                                 <li>About</li>
                                 <li>Contact</li>
-                        </ul>
+                        </ul> */}
                 </div>
         )
 }
